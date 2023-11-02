1c94f44a83430fc4eb597deac9b7e92f46d60244
Bedê <bedegabriel14@gmail.com>
2023-11-02 19:36:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 🫂 BeKind - Plataforma de Ajuda Social
+
+Bem-vindo ao repositório da BeKind! A BeKind é uma plataforma em desenvolvimento que tem como objetivo ajudar moradores de rua e pessoas em situação de vulnerabilidade. Nossa missão é fornecer suporte, recursos e conscientização sobre questões sociais. Este projeto foi criado como parte do meu projeto de conclusão de curso e visa promover a solidariedade, empatia e compaixão em relação às pessoas em situação de vulnerabilidade.
+
+![Logo da BeKind](./assets/Capa.png)
+
+
+## 💙 Objetivo
+
+A BeKind tem como objetivo conectar voluntários, vulneráveis e organizações que desejam fazer a diferença na vida dessas pessoas. Fornecemos uma maneira fácil e eficiente de contribuir para a causa social. Nossa plataforma oferece diversas funcionalidades para atingir esse propósito.
+
+![Website da BeKind](./assets/Website.png)
+
+
+## 💻 Funcionalidades Principais
+
+- **Home**: A página inicial é onde os usuários podem encontrar uma visão geral da BeKind e aprender mais sobre a missão e os objetivos da plataforma.
+
+- **Sobre nós**: Nesta seção, você pode encontrar informações detalhadas sobre a história da BeKind, sua equipe e o propósito da plataforma.
+
+- **Blog**: A página do blog é onde os usuários podem ler informações atualizadas sobre a situação das pessoas em situação de rua, incluindo notícias, histórias de impacto e informações relevantes.
+
+- **Doações**: A página de doações mostra aos usuários as doações em extrema necessidade das ONGs, permitindo que eles contribuam de forma direcionada para ajudar aqueles que mais precisam.
+
+- **Cadastro de Voluntário**: Nesta seção, pessoas interessadas em contribuir como voluntários podem se inscrever.
+
+- **Loja**: A seção da loja permite que você compre produtos relacionados à nossa causa, onde os fundos arrecadados contribuem diretamente para nossos esforços em prol da assistência social.
+
+## 📞 Contato
+
+Se você tiver alguma dúvida, sugestão ou feedback, sinta-se à vontade para entrar em contato conosco:
+
+- E-mail: bekindtcc@gmail.com
+- Instagram: @bekind.initiative
---
+++ js/script.js
@@ -0,0 +1,100 @@
+// Biblioteca de icones "Feather icons"
+feather.replace();
+
+// Animação para os valores do impacto fazerem uma contagem até o valor determinado
+const animateCounters = () => {
+    const counters = document.querySelectorAll('.value');
+    const speed = 500;
+
+    counters.forEach(counter => {
+        const animate = () => {
+            const value = +counter.getAttribute('contador-impacto');
+            const data = +counter.innerText.substring(1);
+            const time = value / speed;
+            
+            if (data < value) {
+                counter.innerText = `+${Math.ceil(data + time)}`;
+                setTimeout(animate, 1);
+            } else {
+                counter.innerText = `+${value}`; 
+            }
+        }
+
+        animate();
+    });
+  };
+
+  // Verificar se a seção "section-apresentacao" está visível ao rolar a página
+  const sectionApresentacao = document.querySelector('.imp3');
+  let animationStarted = false;
+
+  const checkVisibility = () => {
+    const rect = sectionApresentacao.getBoundingClientRect();
+    if (rect.top <= window.innerHeight && !animationStarted) {
+        animateCounters();
+        animationStarted = true;
+    }
+  };
+
+  window.addEventListener('scroll', checkVisibility);
+
+
+ // Verificar se o CPF é válido ou não
+  function formatarCPF(campo) {
+    campo.value = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    if (campo.value.length <= 11) {
+        campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o ponto após os primeiros 3 dígitos
+        campo.value = campo.value.replace(/(\d{3})(\d)/, '$1.$2'); // Coloca o ponto após os próximos 3 dígitos
+        campo.value = campo.value.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Coloca o hífen antes dos últimos 2 dígitos
+    } else {
+        campo.value = campo.value.substring(0, 14); // Limita o campo a 14 caracteres (máscara completa)
+    }
+}
+
+function validarCPF(campo) {
+    var cpf = campo.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    if (cpf.length !== 11) {
+        alert("CPF inválido. O CPF deve conter 11 dígitos.");
+        campo.value = "";
+        return;
+    }
+
+    var soma = 0;
+    var resto;
+
+    for (var i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if ((resto === 10) || (resto === 11)) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+        alert("CPF inválido.");
+        campo.value = "";
+        return;
+    }
+
+    soma = 0;
+
+    for (var i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if ((resto === 10) || (resto === 11)) {
+        resto = 0;
+    }
+
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        alert("CPF inválido.");
+        campo.value = "";
+        return;
+    }
+}
